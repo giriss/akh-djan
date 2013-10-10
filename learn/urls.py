c380cadf18adfa1d57ce67ed9dbbc7bf41ccb5d2
Girish Gopaul <akhile@dr.com>
2013-10-10 00:00:04 -0700

    Update urls.py

--- learn/urls.py
+++ learn/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^lesson1/$', 'learn.views.lesson1', name='lesson1'),
+    url(r'^lesson1/(?P<name>[a-zA-Z]+)/$', 'learn.views.lesson1', name='lesson1'),
 )
